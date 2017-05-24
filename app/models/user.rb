fbfa43d1dea080347bac2bd888a2c7b737d0228d
Lucas Costa <lucascostaa73@gmail.com>
2017-05-24 14:12:47 -0300

    Implementing Acceptance Criteria for user story 01
    US#01

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
 	has_secure_password
 	has_many :products
+	validates_length_of :password, :in => 1..10, :allow_nil => false, :allow_blank => false
+	validates_length_of :name, :in => 3..10, :allow_nil => false, :allow_blank => false
+	validates_format_of :name, :with => /\A[-A-Za-z0-9]+\Z/
 end

7425682b31473806d5bf74a103193a6a1e740b4d
Lucas Costa <lucascostaa73@gmail.com>
2017-06-27 21:16:14 -0300

    Testing products and users

--- test/performance/product_test.rb
+++ test/performance/product_test.rb
@@ -2,12 +2,45 @@ require 'test_helper'
 require 'rails/performance_test_help'
 
 class ProductTest < ActionDispatch::PerformanceTest
-  # Refer to the documentation for all available options
-  # self.profile_options = { runs: 5, metrics: [:wall_time, :memory],
-  #                          output: 'tmp/performance', formats: [:flat] }
+  fixtures :users
+  fixtures :products
 
-  # test "homepage" do
-  #   get '/'
-  # end
-  
+
+  # Refer to the documentation for all available options
+  self.profile_options = { runs: 5, metrics: [:wall_time, :memory, :process_time, :objects] }
+
+
+  def setup
+    # setup user
+    @user_created = User.create name: users(:two).name,
+                  email: users(:two).email,
+                  password: users(:two).password
+
+    #setup product
+    @product_created = Product.create name: products(:two).name,
+                            description: products(:two).description
+
+    #linking user to products
+    @user_created.products << @product_created
+  end
+
+
+
+
+  test 'add product' do
+    Product.create name: products(:one).name,
+                description: products(:one).description
+  end
+
+
+  test 'update product' do
+    @product_created.update name: products(:one).name,
+                description: products(:one).description
+  end
+
+  test 'delete product' do
+    @product_created.delete
+  end
+
+
 end
--- test/performance/user_test.rb
+++ test/performance/user_test.rb
@@ -8,18 +8,29 @@ class UserTest < ActionDispatch::PerformanceTest
   self.profile_options = { runs: 5, metrics: [:wall_time, :memory, :process_time, :objects] }
                         #    output: 'tmp/performance', formats: [:flat] }
 
-  # test "add User" do
-  #   post_via_redirect '/users/new', name: users(:one).name,
-  #                       email: users(:one).email,
-  #                       password: users(:one).password,
-  #                       password_confirmation: users(:one).password_confirmation
-  # end
+  def setup
+    @user_created = User.create name: users(:two).name,
+                  email: users(:two).email,
+                  password: users(:two).password
+  end
 
   test "add User" do
     User.create name: users(:one).name,
                   email: users(:one).email,
-                  password: users(:one).password,
-                  password_confirmation: users(:one).password_confirmation
+                  password: users(:one).password
   end
+
+  test "update User" do
+    # user = User.find(name: users(:two).name)
+    @user_created.update name: users(:one).name,
+                  email: users(:one).email,
+                  password: users(:one).password
+  end
+
+  test "delete User" do
+    @user_created.delete
+  end
+
+
 
 end
